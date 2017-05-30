96b032c008c6cb4c0ce9362fe6a969b48c422672
Raphael FOSSE <raphael.fosse91@gmail.com>
2017-05-30 19:16:08 +0200

    flat controller and routing done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -15,5 +15,39 @@ class FlatsController < ApplicationController
     end
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:category, :address,
+      :city, :service_charges, :deposit, :description, :flat_area,
+      :max_roommmates, :number_pieces, :smoker, :animals, :latitude,
+      :longitude)
+  end
 
 end
