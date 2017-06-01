b1cbed6f1f275227feb885759b4ea6f112f286a9
Alexandre Deniau <alexandre.deniau@outlook.fr>
2017-06-01 12:36:18 +0200

    ok geocoding for services - js maps supprimé dans l app.html.erb car il faut le mettre dans les pages index.flats ou index.services sinon il est apl deux fois et ca fait bugger le serveur

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,12 +1,21 @@
 class ServicesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :new ]
-  def index
-    @services = Service.all
-  end
 
   def show
     @service = Service.find(params[:id])
+    @service_coordinates = { lat: @service.latitude, lng: @service.longitude }
   end
+
+  def index
+    @services = Service.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@services) do |service, marker|
+      marker.lat service.latitude
+      marker.lng service.longitude
+      # marker.infowindow render_to_string(partial: "/services/map_box", locals: { service: service })
+    end
+  end
+
 
   def new
     @service = Service.new
@@ -14,16 +23,15 @@ class ServicesController < ApplicationController
 
   def create
     @service = Service.new(service_params)
-    if @service.save
-      redirect_to service_path(@service)
-    else
-      render new
-    end
+    @service.save
+    redirect_to service_path(@service)
+
   end
 
  private
 
   def service_params
-    params.require(:service).permit(:category, :title, :price_per_hour, :description, :address)
+    params.require(:service).permit(:category, :title, :price_per_hour, :description, :address, :latitude,
+      :longitude)
   end
 end
