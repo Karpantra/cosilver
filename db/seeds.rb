9f05ec957d47d54cd63da825247106d6b8d11824
Gautier Weigert <gautier.weigert@gmail.com>
2017-06-01 11:32:56 +0200

    services seed on construction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'nokogiri'
+
+
+
+html_file = open("https://stootie.com/stoot/services")
+html_doc = Nokogiri::HTML(html_file)
+
+# html_doc.search('#stoots-list .link').each do |element|
+#   puts element.text
+# end
+
+html_doc.search('#stoots-list a .img').each do |element|
+  puts element.attr('href')
+end
+
+# html_doc.search('.price-field').each do |element|
+#   puts element.text
+# end
