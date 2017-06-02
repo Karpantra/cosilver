9ab644a7cf4729ee95c572be24a7a9b3b1e4e6ff
Raphael FOSSE <raphael.fosse91@gmail.com>
2017-06-02 10:57:25 +0200

    Messagerie réparée

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,8 +1,9 @@
 $(function() {
   $('.input-daterange').datepicker({
-      format: "dd/mm/yyyy",
-      weekStart: 1,
-      language: "fr",
-      orientation: "bottom auto",
-      multidate: true
+    format: "dd/mm/yyyy",
+    weekStart: 1,
+    language: "fr",
+    orientation: "bottom auto",
+    multidate: true
   });
+});
