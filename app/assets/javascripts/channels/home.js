0085dc8c3ea75de1d22d3a7537e2f65651cb8ca0
christophe panon <panon_chr@hotmail.com>
2017-06-05 17:26:49 +0200

    work in progress

---
+++ app/assets/javascripts/channels/home.js
@@ -0,0 +1,7 @@
+//   $(document).ready(function()
+//   //Set Down Arrow Button
+//   $('#scrollDownArrow').click(function(e) {
+//     e.preventDefault();
+//     jQuery.scrollTo(".supporting", 1000);
+//   });
+// });
